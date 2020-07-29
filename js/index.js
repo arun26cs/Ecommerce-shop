38a9f0687f97e571242d13922870596e0fd147c5
arun varghese <arun26cs@gmail.com>
2020-07-29 15:37:57 -0400

    index.js commits

--- js/index.js
+++ js/index.js
@@ -16,6 +16,12 @@ $(
 
         $("#register-submit").click(function() {
             console.log("register page submitted");
+            var c_name;
+            /*if($(".form-group #customername").val().trim()){
+                c_name=$(".form-group #customername").val().trim();
+            }else{
+                alert("customer name empty");
+            }*/
 
             var form_register = {
                 customername: $(".form-group #customername").val(),
@@ -27,18 +33,29 @@ $(
             };
             console.log(form_register);
             var string_form_register = JSON.stringify(form_register);
+
             $.ajax({
                 type: 'POST',
                 url: "/customers",
+                contentType: "application/json",
+                dataType: 'json',
                 data: string_form_register,
+                success: function(data) {
 
+                    console.log(data);
+                },
+                failure: function() {
+                    alert("failure");
+                }
 
             });
-
             $(".form-body").fadeIn();
             $(".register-body").fadeOut();
             $(".success").fadeToggle("slow");
+
+
         });
+
 
     }
 );
