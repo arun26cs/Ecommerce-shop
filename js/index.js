df6fb0b2b33f83b7c8802a3701b0083686a5955a
arun varghese <arun26cs@gmail.com>
2020-07-30 03:10:29 -0400

    changes for validation

--- js/index.js
+++ js/index.js
@@ -13,15 +13,10 @@ $(
             }
         );
 
-
+        console.log("offset:" + $("#register-submit").offset().top);
         $("#register-submit").click(function() {
             console.log("register page submitted");
-            var c_name;
-            /*if($(".form-group #customername").val().trim()){
-                c_name=$(".form-group #customername").val().trim();
-            }else{
-                alert("customer name empty");
-            }*/
+            console.log(allGood());
 
             var form_register = {
                 customername: $(".form-group #customername").val(),
@@ -56,6 +51,79 @@ $(
 
         });
 
+        allGood = function() {
+            console.log("eneterd valid");
+            var Good = 0;
+            $("#customername").focus(function() {
+                if (!$("#customername").val().trim() || $("#customername").val() == null) {
+                    $("#customername").addClass("redAlert");
+
+                } else {
+                    $("#customername").removeClass("redAlert");
+                    Good += 1;
+                }
+            });
+            $("#username").focus(function() {
+                if (!$("#username").val().trim() || $("#username").val() == null) {
+                    $("#username").addClass("redAlert");
+
+                } else {
+                    $("#username").removeClass("redAlert");
+                    Good += 1;
+                }
+            });
+            $("#password").focus(function() {
+                if (!$("#password").val().trim() || $("#password").val() == null) {
+                    $("#password").addClass("redAlert");
+
+                } else {
+                    $("#password").removeClass("redAlert");
+                    Good += 1;
+                }
+            });
+            $("#email").focus(function() {
+                console.log("email: ");
+                if (!$("#email").val().trim() || $("#email").val() == null) {
+                    $("#email").addClass("redAlert");
+
+                } else {
+                    $("#email").removeClass("redAlert");
+                    Good += 1;
+                }
+            });
+            $("#phone").focus(function() {
+                if (!$("#phone").val().trim() || $("#phone").val() == null) {
+                    $("#phone").addClass("redAlert");
+
+                } else {
+                    $("#phone").removeClass("redAlert");
+                    Good += 1;
+                }
+            });
+            $("#address").focus(function() {
+                if (!$("#address").val().trim() || $("#address").val() == null) {
+                    $("#address").addClass("redAlert");
+
+                } else {
+                    $("#address").removeClass("redAlert");
+                    Good += 1;
+                }
+                console.log(Good);
+            });
+            console.log($("#customername").val(),
+                $("#username").val(),
+                $("#password").val(),
+                $("#email").val(),
+                $("#phone").val(),
+                $("#address").val());
+            if (Good >= 5) {
+                console.log(Good);
+                return true;
+            } else {
+                console.log(Good);
+                return false;
+            }
+        }
 
     }
 );
